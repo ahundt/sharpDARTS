248c37df590d95be17a3544561b79af5a283011a
shuyao95@gmail.com <shuyao95@gmail.com>
2018-09-10 16:54:12 +0800

    bug fix

--- cnn/utils.py
+++ cnn/utils.py
@@ -126,10 +126,9 @@ class Performance(object):
     self.data = None
 
   def update(self, alphas_normal, alphas_reduce, val_loss):
-    data = np.concatenate([alphas_normal.view(1, -1), 
-                           alphas_reduce.view(1, -1), 
-                           np.array([[val_loss]]).view(1,-1)], 
-                          axis=1)
+    data = np.concatenate([alphas_normal.view(-1), 
+                           alphas_reduce.view(-1), 
+                           np.array([val_loss])]).view(1, -1)
     if self.data is not None:
       self.data = np.concatenate([self.data, data], axis=0)
     else:
