a3fc62bf1a458dc11261cb3b873496e3146d8e53
shuyao95@gmail.com <shuyao95@gmail.com>
2018-09-10 16:58:59 +0800

    bug fix

--- cnn/architect.py
+++ cnn/architect.py
@@ -31,10 +31,11 @@ class Architect(object):
   def step(self, input_train, target_train, input_valid, target_valid, eta, network_optimizer, unrolled):
     self.optimizer.zero_grad()
     if unrolled:
-        self._backward_step_unrolled(input_train, target_train, input_valid, target_valid, eta, network_optimizer)
+        loss = self._backward_step_unrolled(input_train, target_train, input_valid, target_valid, eta, network_optimizer)
     else:
-        self._backward_step(input_valid, target_valid)
+        loss = self._backward_step(input_valid, target_valid)
     self.optimizer.step()
+    return loss
 
   def _backward_step(self, input_valid, target_valid):
     loss = self.model._loss(input_valid, target_valid)
