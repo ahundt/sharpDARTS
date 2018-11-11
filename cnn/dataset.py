ab0f1a9fe2e448e658eccf640eaa0958c51dd420
Andrew Hundt <ATHundt@gmail.com>
2018-11-11 16:42:23 -0500

    dataset.py add missing imports

--- cnn/dataset.py
+++ cnn/dataset.py
@@ -1,4 +1,17 @@
-
+import os
+import sys
+import time
+import glob
+import numpy as np
+import torch
+import utils
+import logging
+import argparse
+import torch.nn as nn
+import genotypes
+import torch.utils
+import torchvision.datasets as dset
+import torch.backends.cudnn as cudnn
 
 CIFAR_CLASSES = 10
 MNIST_CLASSES = 10
