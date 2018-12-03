1344f3d75ea7ec272a8abca98649cc3ab69c2bcf
Andrew Hundt <ATHundt@gmail.com>
2018-12-03 17:26:28 -0500

    operations.py pep8 whitespace fix

--- cnn/operations.py
+++ cnn/operations.py
@@ -2,24 +2,24 @@ import torch
 import torch.nn as nn
 
 OPS = {
-  'none' : lambda C, stride, affine: Zero(stride),
-  'avg_pool_3x3' : lambda C, stride, affine: nn.AvgPool2d(3, stride=stride, padding=1, count_include_pad=False),
-  'max_pool_3x3' : lambda C, stride, affine: nn.MaxPool2d(3, stride=stride, padding=1),
-  'skip_connect' : lambda C, stride, affine: Identity() if stride == 1 else FactorizedReduce(C, C, affine=affine),
-  'sep_conv_3x3' : lambda C, stride, affine: SepConv(C, C, 3, stride, 1, affine=affine),
-  'sep_conv_5x5' : lambda C, stride, affine: SepConv(C, C, 5, stride, 2, affine=affine),
-  'sep_conv_7x7' : lambda C, stride, affine: SepConv(C, C, 7, stride, 3, affine=affine),
-  'dil_conv_3x3' : lambda C, stride, affine: DilConv(C, C, 3, stride, 2, 2, affine=affine),
-  'dil_conv_5x5' : lambda C, stride, affine: DilConv(C, C, 5, stride, 4, 2, affine=affine),
-  'conv_7x1_1x7' : lambda C, stride, affine: nn.Sequential(
+  'none': lambda C, stride, affine: Zero(stride),
+  'avg_pool_3x3': lambda C, stride, affine: nn.AvgPool2d(3, stride=stride, padding=1, count_include_pad=False),
+  'max_pool_3x3': lambda C, stride, affine: nn.MaxPool2d(3, stride=stride, padding=1),
+  'skip_connect': lambda C, stride, affine: Identity() if stride == 1 else FactorizedReduce(C, C, affine=affine),
+  'sep_conv_3x3': lambda C, stride, affine: SepConv(C, C, 3, stride, 1, affine=affine),
+  'sep_conv_5x5': lambda C, stride, affine: SepConv(C, C, 5, stride, 2, affine=affine),
+  'sep_conv_7x7': lambda C, stride, affine: SepConv(C, C, 7, stride, 3, affine=affine),
+  'dil_conv_3x3': lambda C, stride, affine: DilConv(C, C, 3, stride, 2, 2, affine=affine),
+  'dil_conv_5x5': lambda C, stride, affine: DilConv(C, C, 5, stride, 4, 2, affine=affine),
+  'conv_7x1_1x7': lambda C, stride, affine: nn.Sequential(
     nn.ReLU(inplace=False),
-    nn.Conv2d(C, C, (1,7), stride=(1, stride), padding=(0, 3), bias=False),
-    nn.Conv2d(C, C, (7,1), stride=(stride, 1), padding=(3, 0), bias=False),
+    nn.Conv2d(C, C, (1, 7), stride=(1, stride), padding=(0, 3), bias=False),
+    nn.Conv2d(C, C, (7, 1), stride=(stride, 1), padding=(3, 0), bias=False),
     nn.BatchNorm2d(C, affine=affine)
     ),
-  'nor_conv_3x3' : lambda C, stride, affine: ConvBNReLU(C, C, 3, stride, 1, affine=affine),
-  'nor_conv_5x5' : lambda C, stride, affine: ConvBNReLU(C, C, 5, stride, 2, affine=affine),
-  'nor_conv_7x7' : lambda C, stride, affine: ConvBNReLU(C, C, 7, stride, 3, affine=affine),
+  'nor_conv_3x3': lambda C, stride, affine: ConvBNReLU(C, C, 3, stride, 1, affine=affine),
+  'nor_conv_5x5': lambda C, stride, affine: ConvBNReLU(C, C, 5, stride, 2, affine=affine),
+  'nor_conv_7x7': lambda C, stride, affine: ConvBNReLU(C, C, 7, stride, 3, affine=affine),
 }
 
 class ConvBNReLU(nn.Module):
