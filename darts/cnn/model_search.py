13dbc8fa16e64409750d45dad96f599c074a8071
Andrew Hundt <ATHundt@gmail.com>
2018-12-08 16:06:06 -0500

    model_search.py debug weights len

--- darts/cnn/model_search.py
+++ darts/cnn/model_search.py
@@ -38,7 +38,7 @@ class MixedOp(nn.Module):
 
   def forward(self, x, weights):
     # apply all ops with intensity corresponding to their weight
-    # print('weights shape: ' + str(weights.shape) + ' ops shape: ' + len(self.ops_))
+    print('weights shape: ' + str(len(weights)) + ' ops shape: ' + str(len(self.ops_)))
     return sum(w * op(x) for w, op in zip(weights, self._ops))
 
 
