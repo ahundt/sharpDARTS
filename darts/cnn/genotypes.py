8b3fad7a3c3c72cadc5f9b08c5a8daba0e59daed
Andrew Hundt <ATHundt@gmail.com>
2018-12-08 14:23:11 -0500

    genotypes.py add PNASNET

--- darts/cnn/genotypes.py
+++ darts/cnn/genotypes.py
@@ -74,6 +74,36 @@ AmoebaNet = Genotype(
   reduce_concat = [3, 4, 6]
 )
 
+# source: https://github.com/chenxi116/PNASNet.pytorch
+PNASNet = Genotype(
+  normal = [
+    ('sep_conv_5x5', 0),
+    ('max_pool_3x3', 0),
+    ('sep_conv_7x7', 1),
+    ('max_pool_3x3', 1),
+    ('sep_conv_5x5', 1),
+    ('sep_conv_3x3', 1),
+    ('sep_conv_3x3', 4),
+    ('max_pool_3x3', 1),
+    ('sep_conv_3x3', 0),
+    ('skip_connect', 1),
+  ],
+  normal_concat = [2, 3, 4, 5, 6],
+  reduce = [
+    ('sep_conv_5x5', 0),
+    ('max_pool_3x3', 0),
+    ('sep_conv_7x7', 1),
+    ('max_pool_3x3', 1),
+    ('sep_conv_5x5', 1),
+    ('sep_conv_3x3', 1),
+    ('sep_conv_3x3', 4),
+    ('max_pool_3x3', 1),
+    ('sep_conv_3x3', 0),
+    ('skip_connect', 1),
+  ],
+  reduce_concat = [2, 3, 4, 5, 6],
+)
+
 FASHION = Genotype(normal=[('skip_connect', 0), ('sep_conv_3x3', 1), ('skip_connect', 0), ('sep_conv_3x3', 2), ('skip_connect', 0), ('sep_conv_3x3', 2), ('skip_connect', 0), ('dil_conv_3x3', 2)], normal_concat=[2, 3, 4, 5], reduce=[('max_pool_3x3', 0), ('max_pool_3x3', 1), ('avg_pool_3x3', 0), ('skip_connect', 2), ('max_pool_3x3', 0), ('skip_connect', 2), ('avg_pool_3x3', 0), ('skip_connect', 2)], reduce_concat=[2, 3, 4, 5])
 
 DARTS_V1 = Genotype(normal=[('sep_conv_3x3', 1), ('sep_conv_3x3', 0), ('skip_connect', 0), ('sep_conv_3x3', 1), ('skip_connect', 0), ('sep_conv_3x3', 1), ('sep_conv_3x3', 0), ('skip_connect', 2)], normal_concat=[2, 3, 4, 5], reduce=[('max_pool_3x3', 0), ('max_pool_3x3', 1), ('skip_connect', 2), ('max_pool_3x3', 0), ('max_pool_3x3', 0), ('skip_connect', 2), ('skip_connect', 2), ('avg_pool_3x3', 0)], reduce_concat=[2, 3, 4, 5])
