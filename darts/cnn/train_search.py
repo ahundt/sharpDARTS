7df081b81e848b9221ae54abd696d71fcbba0e9b
Andrew Hundt <ATHundt@gmail.com>
2018-12-11 16:05:52 -0500

    train_search.py super simple training mode flag added

--- darts/cnn/train_search.py
+++ darts/cnn/train_search.py
@@ -41,6 +41,7 @@ parser.add_argument('--layers_in_cells', type=int, default=4, help='total number
 parser.add_argument('--reduce_spacing', type=int, default=None, help='Number of layers between each reduction cell. 1 will mean all reduction cells.')
 parser.add_argument('--model_path', type=str, default='saved_models', help='path to save the model')
 parser.add_argument('--cutout', action='store_true', default=False, help='use cutout')
+parser.add_argument('--simple', action='store_true', default=False, help='use a much simpler training regime with no architect')
 parser.add_argument('--cutout_length', type=int, default=16, help='cutout length')
 parser.add_argument('--drop_path_prob', type=float, default=0.3, help='drop path probability')
 parser.add_argument('--save', type=str, default='EXP', help='experiment name')
@@ -85,12 +86,15 @@ def main():
   model = model.cuda()
   logger.info("param size = %fMB", utils.count_parameters_in_MB(model))
 
-  # optimizer = torch.optim.SGD(
-  #     model.parameters(),
-  #     args.learning_rate,
-  #     momentum=args.momentum,
-  #     weight_decay=args.weight_decay)
-  optimizer = Padam(model.parameters(), args.learning_rate)
+  if args.simple:
+    optimizer = Padam([model.parameters(), model.arch_parameters()], args.learning_rate)
+  else:
+    # optimizer = torch.optim.SGD(
+    #     model.parameters(),
+    #     args.learning_rate,
+    #     momentum=args.momentum,
+    #     weight_decay=args.weight_decay)
+    optimizer = Padam(model.parameters(), args.learning_rate)
 
   # Get preprocessing functions (i.e. transforms) to apply on data
   train_transform, valid_transform = utils.get_data_transforms(args)
@@ -100,7 +104,10 @@ def main():
   scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
         optimizer, float(args.epochs), eta_min=args.learning_rate_min)
 
-  architect = Architect(model, args.momentum, args.weight_decay, args.arch_learning_rate, arch_weight_decay=args.arch_weight_decay)
+  if args.simple:
+    architect = None
+  else:
+    architect = Architect(model, args.momentum, args.weight_decay, args.arch_learning_rate, arch_weight_decay=args.arch_weight_decay)
 
   perfor = None
   # perfor = utils.Performance(os.path.join(args.save, 'architecture_performance_history.npy'))
@@ -146,16 +153,20 @@ def train(train_queue, valid_queue, model, architect, criterion, optimizer, lr, 
     target_search = Variable(target_search, requires_grad=False).cuda(async=True)
 
     # define validation loss for analyzing the importance of hyperparameters
-    val_loss = architect.step(input_batch, target, input_search, target_search, lr, optimizer, unrolled=args.unrolled)
-    # add current performance config into performance array
-    # perfor.update(model.alphas_normal, model.alphas_reduce, val_loss)
+    if architect is not None:
+      val_loss = architect.step(input_batch, target, input_search, target_search, lr, optimizer, unrolled=args.unrolled)
+      # add current performance config into performance array
+      # perfor.update(model.alphas_normal, model.alphas_reduce, val_loss)
+    logits = None
 
     optimizer.zero_grad()
     logits = model(input_batch)
     loss = criterion(logits, target)
 
     loss.backward()
-    nn.utils.clip_grad_norm_(model.parameters(), args.grad_clip)
+    if architect is not None:
+      nn.utils.clip_grad_norm_(model.parameters(), args.grad_clip)
+
     optimizer.step()
 
     prec1, prec5 = utils.accuracy(logits, target, topk=(1, 5))
