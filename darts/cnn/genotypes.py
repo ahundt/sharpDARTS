a22384be849be7bbf1c59fa7a7da9062626a9dd4
Andrew Hundt <ATHundt@gmail.com>
2018-12-14 16:57:58 -0500

    genotypes.py aux typo fix

--- darts/cnn/genotypes.py
+++ darts/cnn/genotypes.py
@@ -169,7 +169,8 @@ DARTS_V1 = Genotype(normal=[('sep_conv_3x3', 1), ('sep_conv_3x3', 0), ('skip_con
   start = [],
   start_concat = [],
   end = [],
-  end_concat = [],)
+  end_concat = [],
+  aux = [],)
 DARTS_V2 = Genotype(normal=[('sep_conv_3x3', 0), ('sep_conv_3x3', 1), ('sep_conv_3x3', 0), ('sep_conv_3x3', 1), ('sep_conv_3x3', 1), ('skip_connect', 0), ('skip_connect', 0), ('dil_conv_3x3', 2)], normal_concat=[2, 3, 4, 5], reduce=[('max_pool_3x3', 0), ('max_pool_3x3', 1), ('skip_connect', 2), ('max_pool_3x3', 1), ('max_pool_3x3', 0), ('skip_connect', 2), ('skip_connect', 2), ('max_pool_3x3', 1)], reduce_concat=[2, 3, 4, 5],
   start = [],
   start_concat = [],
