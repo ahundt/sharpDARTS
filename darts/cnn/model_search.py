f5610521322280cc6a62b88e35f077dd29d9a93b
Andrew Hundt <ATHundt@gmail.com>
2018-12-14 17:22:13 -0500

    model_search.py print fixed

--- darts/cnn/model_search.py
+++ darts/cnn/model_search.py
@@ -98,7 +98,7 @@ class MixedAux(nn.Module):
     for w, op, x in zip(weights, self._ops, xs):
       out = self.global_pooling(x)
       logits += [w * op(out.view(out.size(0), -1))]
-    print('MixedAux logits: ' + logits)
+    print('MixedAux logits: ' + str(logits))
     return sum(logits)
 
   def genotype(self):
