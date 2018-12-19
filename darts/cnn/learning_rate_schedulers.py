d50b98e836f143e61b10f029fda2da682edc4687
Andrew Hundt <ATHundt@gmail.com>
2018-12-18 20:27:15 -0500

    learning_rate_schedulers.py steeper cycle decay factor

--- darts/cnn/learning_rate_schedulers.py
+++ darts/cnn/learning_rate_schedulers.py
@@ -32,7 +32,7 @@ class CosineWithRestarts(torch.optim.lr_scheduler._LRScheduler):  # pylint: disa
                  eta_min=0.,
                  last_epoch=-1,
                  factor=1.,
-                 cycle_decay_factor=0.9):
+                 cycle_decay_factor=0.6):
         assert t_max > 0
         assert eta_min >= 0
         if t_max == 1 and factor == 1:
@@ -45,6 +45,7 @@ class CosineWithRestarts(torch.optim.lr_scheduler._LRScheduler):  # pylint: disa
         self._cycle_counter = 0
         self._cycle_factor = 1.
         self._cycle_decay_factor = cycle_decay_factor
+        self._restart_counter = 0
         self._updated_cycle_len = t_max
         self._initialized = False
         super(CosineWithRestarts, self).__init__(optimizer, last_epoch)
@@ -69,7 +70,7 @@ class CosineWithRestarts(torch.optim.lr_scheduler._LRScheduler):  # pylint: disa
                                 (self._cycle_counter % self._updated_cycle_len) /
                                 self._updated_cycle_len
                         ) + 1
-                ) * (self._cycle_decay_factor ** (self._cycle_factor - 1.0))
+                ) * (self._cycle_decay_factor ** self._restart_counter)
                 for lr in self.base_lrs
         ]
 
@@ -79,5 +80,6 @@ class CosineWithRestarts(torch.optim.lr_scheduler._LRScheduler):  # pylint: disa
             self._cycle_counter = 0
             self._updated_cycle_len = int(self._cycle_factor * self.t_max)
             self._last_restart = step
+            self._restart_counter += 1
 
         return lrs
