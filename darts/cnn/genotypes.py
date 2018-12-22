b2f63064402eb470a2ce44714ad819fabb7f6ac4
Andrew Hundt <ATHundt@gmail.com>
2018-12-22 12:08:27 -0500

    genotypes.py add HESSIAN_CHOKE_FLOOD

--- darts/cnn/genotypes.py
+++ darts/cnn/genotypes.py
@@ -166,6 +166,16 @@ CHOKE_FLOOD = Genotype(normal=[('skip_connect', 0), ('skip_connect', 1), ('max_p
   end = [],
   end_concat = [], aux=[0.019386334344744682, 0.021869726479053497, 0.019505096599459648, 0.020072733983397484, 0.02058151178061962, 0.20746095478534698, 0.43517687916755676, 0.2559467852115631])
 
+# really long really slow second derivative training run:
+# search-20181211-142658-choke_flood_primitives-cifar10
+# 2018_12_21_10_55_01 valid_acc 92.640000 2018_12_21_10_54_34 train_acc 99.628889 2018_12_21_10_55_01 epoch 85 lr 5.544224e-03
+HESSIAN_CHOKE_FLOOD = Genotype(normal=[('skip_connect', 0), ('skip_connect', 1), ('skip_connect', 1), ('flood_conv_3x3', 2), ('skip_connect', 1), ('skip_connect', 0), ('max_pool_3x3', 3), ('dil_choke_conv_3x3', 0)], normal_concat=range(2, 6), reduce=[('max_pool_3x3', 1), ('flood_conv_3x3', 0), ('skip_connect', 1), ('skip_connect', 2), ('choke_conv_3x3', 0), ('dil_conv_3x3', 1), ('skip_connect', 2), ('max_pool_3x3', 1)], reduce_concat=range(2, 6),
+  start = [],
+  start_concat = [],
+  end = [],
+  end_concat = [],
+  aux = [],)
+
 FASHION = Genotype(normal=[('skip_connect', 0), ('sep_conv_3x3', 1), ('skip_connect', 0), ('sep_conv_3x3', 2), ('skip_connect', 0), ('sep_conv_3x3', 2), ('skip_connect', 0), ('dil_conv_3x3', 2)], normal_concat=[2, 3, 4, 5], reduce=[('max_pool_3x3', 0), ('max_pool_3x3', 1), ('avg_pool_3x3', 0), ('skip_connect', 2), ('max_pool_3x3', 0), ('skip_connect', 2), ('avg_pool_3x3', 0), ('skip_connect', 2)], reduce_concat=[2, 3, 4, 5],
   start = [],
   start_concat = [],
