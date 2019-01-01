04eed152273f61dabfe486749b3ee21b31f2cdac
Andrew Hundt <ATHundt@gmail.com>
2018-12-31 18:42:15 -0800

    train.py enable reverting to earlier ops config definitions

--- darts/cnn/train.py
+++ darts/cnn/train.py
@@ -19,6 +19,7 @@ from tqdm import tqdm
 from Padam import Padam
 import darts.cnn.model as model
 import dataset
+import operations
 from learning_rate_schedulers import CosineWithRestarts
 
 
@@ -75,7 +76,10 @@ def main():
     torch.cuda.manual_seed(args.seed)
   logger.info('gpu device = %d' % args.gpu)
   logger.info("args = %s", args)
+  # load the correct ops dictionary
+  operations.OPS = eval("operations.%s" % args.ops)
 
+  # load the architecture genotype
   genotype = eval("genotypes.%s" % args.arch)
   number_of_classes = dataset.class_dict[args.dataset]
   in_channels = dataset.inp_channel_dict[args.dataset]
