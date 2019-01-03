12a8359050c4f3aef0d24c548c41f2930d6a385b
Andrew Hundt <ATHundt@gmail.com>
2019-01-02 17:35:17 -0800

    train.py utils.py try adding only printout & logging changes for comparison

--- cnn/train.py
+++ cnn/train.py
@@ -2,6 +2,7 @@ import os
 import sys
 import time
 import glob
+import json
 import numpy as np
 import torch
 import utils
@@ -17,41 +18,46 @@ from torch.autograd import Variable
 from model import NetworkCIFAR as Network
 
 
-parser = argparse.ArgumentParser("cifar")
+parser = argparse.ArgumentParser("Common Argument Parser")
 parser.add_argument('--data', type=str, default='../data', help='location of the data corpus')
-parser.add_argument('--batch_size', type=int, default=96, help='batch size')
+parser.add_argument('--dataset', type=str, default='cifar10', help='which dataset:\
+                    cifar10, mnist, emnist, fashion, svhn, stl10, devanagari')
+parser.add_argument('--batch_size', type=int, default=64, help='batch size')
 parser.add_argument('--learning_rate', type=float, default=0.025, help='init learning rate')
+parser.add_argument('--learning_rate_min', type=float, default=0.0000001, help='min learning rate')
 parser.add_argument('--momentum', type=float, default=0.9, help='momentum')
 parser.add_argument('--weight_decay', type=float, default=3e-4, help='weight decay')
+parser.add_argument('--partial', default=1/8, type=float, help='partially adaptive parameter p in Padam')
 parser.add_argument('--report_freq', type=float, default=50, help='report frequency')
 parser.add_argument('--gpu', type=int, default=0, help='gpu device id')
-parser.add_argument('--epochs', type=int, default=600, help='num of training epochs')
+parser.add_argument('--epochs', type=int, default=1000, help='num of training epochs')
+parser.add_argument('--warm_restarts', type=int, default=20, help='warm restarts of cosine annealing')
 parser.add_argument('--init_channels', type=int, default=36, help='num of init channels')
 parser.add_argument('--layers', type=int, default=20, help='total number of layers')
 parser.add_argument('--model_path', type=str, default='saved_models', help='path to save the model')
 parser.add_argument('--auxiliary', action='store_true', default=False, help='use auxiliary tower')
+parser.add_argument('--mixed_auxiliary', action='store_true', default=False, help='Learn weights for auxiliary networks during training. Overrides auxiliary flag')
 parser.add_argument('--auxiliary_weight', type=float, default=0.4, help='weight for auxiliary loss')
 parser.add_argument('--cutout', action='store_true', default=False, help='use cutout')
 parser.add_argument('--cutout_length', type=int, default=16, help='cutout length')
+parser.add_argument('--random_eraser', action='store_true', default=False, help='use random eraser')
 parser.add_argument('--drop_path_prob', type=float, default=0.2, help='drop path probability')
 parser.add_argument('--save', type=str, default='EXP', help='experiment name')
 parser.add_argument('--seed', type=int, default=0, help='random seed')
 parser.add_argument('--arch', type=str, default='DARTS', help='which architecture to use')
+parser.add_argument('--ops', type=str, default='OPS', help='which operations to use, options are OPS and DARTS_OPS')
+parser.add_argument('--optimizer', type=str, default='sgd', help='which optimizer to use, options are padam and sgd')
 parser.add_argument('--grad_clip', type=float, default=5, help='gradient clipping')
 args = parser.parse_args()
 
-args.save = 'eval-{}-{}'.format(args.save, time.strftime("%Y%m%d-%H%M%S"))
+args.save = 'eval-{}-{}-{}-{}'.format(time.strftime("%Y%m%d-%H%M%S"), args.save, args.dataset, args.arch)
 utils.create_exp_dir(args.save, scripts_to_save=glob.glob('*.py'))
 
-log_format = '%(asctime)s %(message)s'
-logging.basicConfig(stream=sys.stdout, level=logging.INFO,
-    format=log_format, datefmt='%m/%d %I:%M:%S %p')
-fh = logging.FileHandler(os.path.join(args.save, 'log.txt'))
-fh.setFormatter(logging.Formatter(log_format))
-logging.getLogger().addHandler(fh)
-
-CIFAR_CLASSES = 10
-
+log_file_path = os.path.join(args.save, 'log.txt')
+logger = utils.logging_setup(log_file_path)
+params_path = os.path.join(args.save, 'commandline_args.json')
+with open(params_path, 'w') as f:
+    json.dump(vars(args), f)
 
 def main():
   if not torch.cuda.is_available():
@@ -94,77 +100,94 @@ def main():
 
   scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, float(args.epochs))
 
-  for epoch in range(args.epochs):
+  prog_epoch = tqdm(range(args.epochs), dynamic_ncols=True)
+  best_valid_acc = 0.0
+  best_epoch = 0
+  weights_file = os.path.join(args.save, 'weights.pt')
+  for epoch in prog_epoch:
     scheduler.step()
-    logging.info('epoch %d lr %e', epoch, scheduler.get_lr()[0])
-    model.drop_path_prob = args.drop_path_prob * epoch / args.epochs
+    cnn_model.drop_path_prob = args.drop_path_prob * epoch / args.epochs
 
-    train_acc, train_obj = train(train_queue, model, criterion, optimizer)
-    logging.info('train_acc %f', train_acc)
+    train_acc, train_obj = train(train_queue, cnn_model, criterion, optimizer)
 
-    valid_acc, valid_obj = infer(valid_queue, model, criterion)
-    logging.info('valid_acc %f', valid_acc)
+    valid_acc, valid_obj = infer(valid_queue, cnn_model, criterion)
 
-    utils.save(model, os.path.join(args.save, 'weights.pt'))
+    if valid_acc > best_valid_acc:
+      # new best epoch, save weights
+      utils.save(cnn_model, weights_file)
+      best_epoch = epoch
+      best_valid_acc = valid_acc
+    # else:
+    #   # not best epoch, load best weights
+    #   utils.load(cnn_model, weights_file)
+    logger.info('epoch, %d, train_acc, %f, valid_acc, %f, train_loss, %f, valid_loss, %f, lr, %e, best_epoch, %d, best_valid_acc, %f',
+                epoch, train_acc, valid_acc, train_obj, valid_obj, scheduler.get_lr()[0], best_epoch, best_valid_acc)
+  logger.info('Training of Final Model Complete! Save dir: ' + str(args.save))
 
 
-def train(train_queue, model, criterion, optimizer):
+def train(train_queue, cnn_model, criterion, optimizer):
   objs = utils.AvgrageMeter()
   top1 = utils.AvgrageMeter()
   top5 = utils.AvgrageMeter()
-  model.train()
+  cnn_model.train()
 
-  for step, (input, target) in enumerate(train_queue):
-    input = Variable(input).cuda()
-    target = Variable(target).cuda(async=True)
+  progbar = tqdm(train_queue, dynamic_ncols=True)
+  for step, (input_batch, target) in enumerate(progbar):
+    input_batch = Variable(input_batch)
+    target = Variable(target)
+    if torch.cuda.is_available():
+      input_batch = input_batch.cuda()
+      target = target.cuda(async=True)
 
     optimizer.zero_grad()
-    logits, logits_aux = model(input)
+    logits, logits_aux = cnn_model(input_batch)
     loss = criterion(logits, target)
-    if args.auxiliary:
+    if logits_aux is not None and args.auxiliary:
       loss_aux = criterion(logits_aux, target)
-      loss += args.auxiliary_weight*loss_aux
+      loss += args.auxiliary_weight * loss_aux
     loss.backward()
-    nn.utils.clip_grad_norm(model.parameters(), args.grad_clip)
+    nn.utils.clip_grad_norm_(cnn_model.parameters(), args.grad_clip)
+    if cnn_model.auxs is not None:
+      # clip the aux weights even more so they don't jump too quickly
+      nn.utils.clip_grad_norm_(cnn_model.auxs.alphas, args.grad_clip/10)
     optimizer.step()
 
     prec1, prec5 = utils.accuracy(logits, target, topk=(1, 5))
-    n = input.size(0)
+    n = input_batch.size(0)
     objs.update(loss.data.item(), n)
     top1.update(prec1.data.item(), n)
     top5.update(prec5.data.item(), n)
 
-    if step % args.report_freq == 0:
-      logging.info('train %03d %e %f %f', step, objs.avg, top1.avg, top5.avg)
+    progbar.set_description('Training loss: {0:9.5f}, top 1: {1:5.2f}, top 5: {2:5.2f} progress'.format(objs.avg, top1.avg, top5.avg))
 
   return top1.avg, objs.avg
 
 
-def infer(valid_queue, model, criterion):
+def infer(valid_queue, cnn_model, criterion):
   objs = utils.AvgrageMeter()
   top1 = utils.AvgrageMeter()
   top5 = utils.AvgrageMeter()
-  model.eval()
+  cnn_model.eval()
 
-  for step, (input, target) in enumerate(valid_queue):
-    input = Variable(input, volatile=True).cuda()
-    target = Variable(target, volatile=True).cuda(async=True)
+  with torch.no_grad():
+    progbar = tqdm(valid_queue, dynamic_ncols=True)
+    for step, (input_batch, target) in enumerate(progbar):
+      input_batch = Variable(input_batch).cuda()
+      target = Variable(target).cuda(async=True)
 
-    logits, _ = model(input)
-    loss = criterion(logits, target)
+      logits, _ = cnn_model(input_batch)
+      loss = criterion(logits, target)
 
-    prec1, prec5 = utils.accuracy(logits, target, topk=(1, 5))
-    n = input.size(0)
-    objs.update(loss.data.item(), n)
-    top1.update(prec1.data.item(), n)
-    top5.update(prec5.data.item(), n)
-
-    if step % args.report_freq == 0:
-      logging.info('valid %03d %e %f %f', step, objs.avg, top1.avg, top5.avg)
+      prec1, prec5 = utils.accuracy(logits, target, topk=(1, 5))
+      n = input_batch.size(0)
+      objs.update(loss.data.item(), n)
+      top1.update(prec1.data.item(), n)
+      top5.update(prec5.data.item(), n)
+      progbar.set_description('Validation step: {0}, loss: {1:9.5f}, top 1: {2:5.2f} top 5: {3:5.2f} progress'.format(step, objs.avg, top1.avg, top5.avg))
 
   return top1.avg, objs.avg
 
 
 if __name__ == '__main__':
-  main() 
+  main()
 
--- cnn/utils.py
+++ cnn/utils.py
@@ -1,9 +1,58 @@
 import os
 import numpy as np
+import logging
 import torch
 import shutil
 import torchvision.transforms as transforms
 from torch.autograd import Variable
+import torch.nn.functional as F
+try:
+    import fanova
+except ImportError:
+    print('fanova not available, skipping components ranking hyperparam importance, try:'
+          '    pip install fanova --user --upgrade'
+          'or follow the instructions at https://github.com/automl/fanova')
+    fanova = None
+
+from tqdm import tqdm
+import colorlog
+
+class TqdmHandler(logging.StreamHandler):
+    def __init__(self):
+        logging.StreamHandler.__init__(self)
+
+    def emit(self, record):
+        msg = self.format(record)
+        tqdm.write(msg)
+
+def logging_setup(log_file_path):
+    """ setup logging to a file and support for tqdm progress bar
+
+      log_file_path: path to log file which will be created as a txt to output printed information
+    """
+    # setup logging for tqdm compatibility
+    # based on https://github.com/tqdm/tqdm/issues/193#issuecomment-232887740
+    logger = colorlog.getLogger("SQUARE")
+    logger.setLevel(logging.DEBUG)
+    handler = TqdmHandler()
+    log_format = colorlog.ColoredFormatter(
+        # '%(log_color)s%(name)s | %(asctime)s | %(levelname)s | %(message)s',
+        '%(asctime)s %(message)s',
+        datefmt='%Y_%m_%d_%H_%M_%S',
+        log_colors={
+            'DEBUG': 'cyan',
+            'INFO': 'white',
+            'SUCCESS:': 'green',
+            'WARNING': 'yellow',
+            'ERROR': 'red',
+            'CRITICAL': 'red,bg_white'},)
+    handler.setFormatter(log_format)
+
+    logger.addHandler(handler)
+    fh = logging.FileHandler(log_file_path)
+    fh.setFormatter(log_format)
+    logger.addHandler(fh)
+    return logger
 
 
 class AvgrageMeter(object):
@@ -37,6 +86,44 @@ def accuracy(output, target, topk=(1,)):
   return res
 
 
+def random_eraser(input_img, p=0.66, s_l=0.02, s_h=0.4, r_1=0.3, r_2=1/0.3, v_l=0, v_h=255, pixel_level=True):
+    """ Cutout and random erasing algorithms for data augmentation
+
+    source:
+    https://github.com/yu4u/cutout-random-erasing/blob/master/random_eraser.py
+
+    modified for batch, channel, height, width dimension order, and so there are no while loop delays.
+    """
+    img_c, img_h, img_w = input_img.shape
+    # print('input_img.shape' + str(input_img.shape))
+    p_1 = np.random.rand()
+
+    if p_1 > p:
+        return input_img
+
+    s = np.random.uniform(s_l, s_h) * img_h * img_w
+    r = np.random.uniform(r_1, r_2)
+    w = int(np.sqrt(s / r))
+    h = int(np.sqrt(s * r))
+    left = np.random.randint(0, img_w)
+    top = np.random.randint(0, img_h)
+    # ensure boundaries fit in the image border
+    w = np.clip(w, 0, img_w - left)
+    h = np.clip(h, 0, img_h - top)
+
+    if pixel_level:
+        c = np.random.uniform(v_l, v_h, (img_c, h, w))
+    else:
+        c = np.random.uniform(v_l, v_h)
+
+    c = torch.from_numpy(c)
+
+    # print('c.shape' + str(c.shape))
+    input_img[:, top:top + h, left:left + w] = c
+
+    return input_img
+
+
 class Cutout(object):
     def __init__(self, length):
         self.length = length
@@ -59,6 +146,26 @@ class Cutout(object):
         return img
 
 
+# Function to fetch the transforms based on the dataset
+def get_data_transforms(args):
+  print("Getting",args.dataset,"Transforms")
+  if args.dataset == 'cifar10':
+    return _data_transforms_cifar10(args)
+  if args.dataset == 'mnist':
+    return _data_transforms_mnist(args)
+  if args.dataset == 'emnist':
+    return _data_transforms_emnist(args)
+  if args.dataset == 'fashion':
+    return _data_transforms_fashion(args)
+  if args.dataset == 'svhn':
+    return _data_transforms_svhn(args)
+  if args.dataset == 'stl10':
+    return _data_transforms_stl10(args)
+  if args.dataset == 'devanagari':
+    return _data_transforms_devanagari(args)
+  assert False, "Cannot get Transform for dataset"
+
+# Transform defined for cifar-10
 def _data_transforms_cifar10(args):
   CIFAR_MEAN = [0.49139968, 0.48215827, 0.44653124]
   CIFAR_STD = [0.24703233, 0.24348505, 0.26158768]
@@ -69,12 +176,152 @@ def _data_transforms_cifar10(args):
     transforms.ToTensor(),
     transforms.Normalize(CIFAR_MEAN, CIFAR_STD),
   ])
+  if args.random_eraser:
+    train_transform.transforms.append(random_eraser)
   if args.cutout:
     train_transform.transforms.append(Cutout(args.cutout_length))
 
   valid_transform = transforms.Compose([
     transforms.ToTensor(),
     transforms.Normalize(CIFAR_MEAN, CIFAR_STD),
+    ])
+  return train_transform, valid_transform
+
+
+# Transform defined for mnist
+def _data_transforms_mnist(args):
+  MNIST_MEAN = (0.1307,)
+  MNIST_STD = (0.3081,)
+
+  train_transform = transforms.Compose([
+    transforms.RandomCrop(28, padding=4),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize(MNIST_MEAN, MNIST_STD),
+  ])
+  if args.random_eraser:
+    train_transform.transforms.append(random_eraser)
+  if args.cutout:
+    train_transform.transforms.append(Cutout(args.cutout_length))
+
+  valid_transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize(MNIST_MEAN, MNIST_STD),
+    ])
+  return train_transform, valid_transform
+
+
+# Transform defined for fashion mnist
+def _data_transforms_fashion(args):
+  FASHION_MEAN = (0.2860405969887955,)
+  FASHION_STD = (0.35302424825650003,)
+
+  train_transform = transforms.Compose([
+    transforms.RandomCrop(28, padding=4),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize(FASHION_MEAN, FASHION_STD),
+  ])
+  if args.random_eraser:
+    train_transform.transforms.append(random_eraser)
+  if args.cutout:
+    train_transform.transforms.append(Cutout(args.cutout_length))
+
+  valid_transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize(FASHION_MEAN, FASHION_STD),
+    ])
+  return train_transform, valid_transform
+
+
+# Transform defined for emnist
+def _data_transforms_emnist(args):
+  EMNIST_MEAN = (0.17510417052459282,)
+  EMNIST_STD = (0.33323714976320795,)
+
+  train_transform = transforms.Compose([
+    transforms.RandomCrop(28, padding=4),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize(EMNIST_MEAN, EMNIST_STD),
+  ])
+  if args.random_eraser:
+    train_transform.transforms.append(random_eraser)
+  if args.cutout:
+    train_transform.transforms.append(Cutout(args.cutout_length))
+
+  valid_transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize(EMNIST_MEAN, EMNIST_STD),
+    ])
+  return train_transform, valid_transform
+
+
+# Transform defined for svhn
+def _data_transforms_svhn(args):
+  SVHN_MEAN = [ 0.4376821,   0.4437697,   0.47280442]
+  SVHN_STD = [ 0.19803012,  0.20101562,  0.19703614]
+
+  train_transform = transforms.Compose([
+    transforms.RandomCrop(32, padding=4),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize(SVHN_MEAN, SVHN_STD),
+  ])
+  if args.random_eraser:
+    train_transform.transforms.append(random_eraser)
+  if args.cutout:
+    train_transform.transforms.append(Cutout(args.cutout_length))
+
+  valid_transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize(SVHN_MEAN, SVHN_STD),
+    ])
+  return train_transform, valid_transform
+
+
+# Transform defined for stl10
+def _data_transforms_stl10(args):
+  STL_MEAN = [ 0.44671062,  0.43980984,  0.40664645]
+  STL_STD = [ 0.26034098,  0.25657727,  0.27126738]
+
+  train_transform = transforms.Compose([
+    transforms.RandomCrop(96, padding=4),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize(STL_MEAN, STL_STD),
+  ])
+  if args.random_eraser:
+    train_transform.transforms.append(random_eraser)
+  if args.cutout:
+    train_transform.transforms.append(Cutout(args.cutout_length))
+
+  valid_transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize(STL_MEAN, STL_STD),
+    ])
+  return train_transform, valid_transform
+
+
+# Transform defined for devanagari hand written symbols
+def _data_transforms_devanagari(args):
+  DEVANAGARI_MEAN = (0.240004663268,)
+  DEVANAGARI_STD = (0.386530114768,)
+
+  train_transform = transforms.Compose([
+    transforms.RandomCrop(32, padding=2), #Already has padding 2 and size is 32x32
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize(DEVANAGARI_MEAN, DEVANAGARI_STD),
+  ])
+  if args.random_eraser:
+    train_transform.transforms.append(random_eraser)
+  if args.cutout:
+    train_transform.transforms.append(Cutout(args.cutout_length))
+
+  valid_transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize(DEVANAGARI_MEAN, DEVANAGARI_STD),
     ])
   return train_transform, valid_transform
 
@@ -119,3 +366,67 @@ def create_exp_dir(path, scripts_to_save=None):
       dst_file = os.path.join(path, 'scripts', os.path.basename(script))
       shutil.copyfile(script, dst_file)
 
+
+class Performance(object):
+  def __init__(self, path):
+    self.path = path
+    self.data = None
+
+  def update(self, alphas_normal, alphas_reduce, val_loss):
+    a_normal = F.softmax(alphas_normal, dim=-1)
+    # if fanova is None:
+    #   print("alpha normal size: ", a_normal.data.size())
+    a_reduce = F.softmax(alphas_reduce, dim=-1)
+    # if fanova is None:
+    #   print("alpha reduce size: ", a_reduce.data.size())
+    data = np.concatenate([a_normal.data.view(-1),
+                           a_reduce.data.view(-1),
+                           np.array([val_loss.data])]).reshape(1,-1)
+    if self.data is not None:
+      self.data = np.concatenate([self.data, data], axis=0)
+    else:
+      self.data = data
+
+  def save(self):
+    np.save(self.path, self.data)
+
+def importance(path, config):
+  assert os.path.exists(path), 'File %s does not exist' % path
+  assert isinstance(config, dict), 'Input argument config is wrong'
+  if fanova is None:
+        print('fanova is disabled, skipping hyperparameter importance estimate')
+        return []
+
+  data = np.load(path)
+  X = data[:, :-1].astype(np.double)
+  Y = data[:, -1].astype(np.double)
+  n_data, n_params = X.shape
+  print(X.shape)
+  imps = []
+  # TODO: make dims, max_cols more clear & automatically deduce them, may be related to search space
+  dims = (10, )
+  max_cols = 50
+
+  if config['mode'] == 'incremental':
+    interval = config['interval']
+    for i in range(n_data // interval):
+      print('Iteration %d: \n' %i)
+      f = fanova.fANOVA(X[:(i+1)*interval, :max_cols], Y[:(i+1)*interval])
+      imp_dic = f.quantify_importance(dims)
+      print(imp_dic)
+      imps.append(imp_dic)
+  elif config['mode'] == 'fixed':
+    interval = config['interval']
+    for i in range(n_data // interval):
+      print('Iteration %d: \n' %i)
+      f = fanova.fANOVA(X[i*interval:(i+1)*interval, :max_cols], Y[i*interval:(i+1)*interval])
+      imp_dic = f.quantify_importance(dims)
+      print(imp_dic)
+      imps.append(imp_dic)
+  return imps
+
+# if __name__ == '__main__':
+#   path = '/home/zero/Downloads/cifar10_performance.npy'
+#   config = {'mode': 'fixed', 'interval': 1000}
+#   # config = {'mode': 'incremental', 'interval': 1000}
+#   imps = importance(path, config)
