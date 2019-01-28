7fe62f1709b38fd249d77a460d9f276fbdee5768
Andrew Hundt <ATHundt@gmail.com>
2019-01-27 22:38:21 -0500

    cnn/utils.py flops count typo fix

--- cnn/utils.py
+++ cnn/utils.py
@@ -371,7 +371,7 @@ def count_model_flops(cnn_model, data_shape=[1, 3, 32, 32]):
   cnn_model_flops = cnn_model
   cnn_model_flops = flops_counter.add_flops_counting_methods(cnn_model)
   batch = torch.zeros(data_shape)
-  cnn_model_flops.eval.start_flops_count()
+  cnn_model_flops.eval().start_flops_count()
   out = cnn_model_flops(batch)
   cnn_model_flops.stop_flops_count()
   flops_str = flops_counter.flops_to_string(model.compute_average_flops_cost())
