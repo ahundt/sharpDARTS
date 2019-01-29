6e0c94e1153b84997fb73c154003491e354882ee
Andrew Hundt <ATHundt@gmail.com>
2019-01-29 16:57:09 -0500

    cnn/main_fp16_optimizer.py str typo fix

--- cnn/main_fp16_optimizer.py
+++ cnn/main_fp16_optimizer.py
@@ -60,7 +60,7 @@ parser.add_argument('-j', '--workers', default=4, type=int, metavar='N',
                     help='number of data loading workers (default: 4)')
 parser.add_argument('--epochs', default=300, type=int, metavar='N',
                     help='number of total epochs to run (default: 300)')
-parser.add_argument('--start_epoch, default=0, type=int, metavar='N',
+parser.add_argument('--start_epoch', default=0, type=int, metavar='N',
                     help='manual epoch number (useful on restarts)')
 parser.add_argument('-b', '--batch-size', default=256, type=int,
                     metavar='N', help='mini-batch size per process (default: 256)')
