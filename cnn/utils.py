cececb1e5be8665faab111f58ea0949537d72665
Andrew Hundt <ATHundt@gmail.com>
2019-02-02 02:35:43 -0500

    cnn/* json dump fix

--- cnn/utils.py
+++ cnn/utils.py
@@ -20,6 +20,26 @@ import autoaugment
 import flops_counter
 
 
+class NumpyEncoder(json.JSONEncoder):
+    """ json encoder for numpy types
+
+    source: https://stackoverflow.com/a/49677241/99379
+    """
+    def default(self, obj):
+        if isinstance(obj,
+            (np.int_, np.intc, np.intp, np.int8,
+             np.int16, np.int32, np.int64, np.uint8,
+             np.uint16, np.uint32, np.uint64)):
+            return int(obj)
+        elif isinstance(obj,
+           (np.float_, np.float16, np.float32,
+            np.float64)):
+            return float(obj)
+        elif isinstance(obj, (np.ndarray,)):
+            return obj.tolist()
+        return json.JSONEncoder.default(self, obj)
+
+
 def tqdm_stats(progbar, prefix=''):
   """ Very brittle function to extract timing stats from tqdm.
   Replace when https://github.com/tqdm/tqdm/issues/562 is resolved.
