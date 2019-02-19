fc8ef6f971811fbefb33f5c6761459f8fa234aa4
Chia-Hung "Rexxar" Lin <rexxar.lin@gmail.com>
2019-02-18 20:38:48 -0500

    train_costar: Change example command

--- cnn/train_costar.py
+++ cnn/train_costar.py
@@ -10,7 +10,7 @@
 # # note that --nproc_per_node is NUM_GPUS.
 # # Can add --sync_bn to sync bachnorm values if batch size is "very small" but note this also reduces img/s by ~10%.
 #
-# Example cifar10 command:
+# Example command:
 #
 #    export CUDA_VISIBLE_DEVICES="2" && python3 train_costar.py --auxiliary --cutout --batch_size 128 --epochs 200 --save `git rev-parse --short HEAD` --epochs 300 --arch SHARP_DARTS --mid_channels 32 --init_channels 36 --wd 0.0003 --lr_power_annealing_exponent_order 2 --learning_rate_min 0.0005 --learning_rate 0.05 --set_name blocks_only --subset_name success_only --feature_mode all_features --data ~/.keras/datasets/costar_block_stacking_dataset_v0.4
 import argparse
