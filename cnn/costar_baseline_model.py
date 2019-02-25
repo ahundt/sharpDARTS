5c2f30b4e45e3457e2fcdb3629ae06965486e3b2
Chia-Hung "Rexxar" Lin <rexxar.lin@gmail.com>
2019-02-24 21:57:46 -0500

    Move NetworkCOSTAR to costar_baseline_model.py

--- cnn/costar_baseline_model.py
+++ cnn/costar_baseline_model.py
@@ -143,3 +143,74 @@ class NetworkNASCOSTAR(nn.Module):
     out = self.global_pooling(s1)
     logits = self.classifier(out.view(out.size(0), -1))
     return logits, logits_aux
+
+
+class NetworkCOSTAR(nn.Module):
+
+  def __init__(self, C, num_classes, layers, auxiliary, genotype, in_channels=6, reduce_spacing=None,
+               mixed_aux=False, op_dict=None, C_mid=None, stem_multiplier=3, vector_size=15):
+    super(NetworkCOSTAR, self).__init__()
+    self._layers = layers
+    self._auxiliary = auxiliary
+    self._in_channels = in_channels
+    self._vector_size = vector_size
+    self.drop_path_prob = 0.
+
+    self.stem0 = nn.Sequential(
+      nn.Conv2d(in_channels, C // 2, kernel_size=3, stride=2, padding=1, bias=False),
+      nn.BatchNorm2d(C // 2),
+      nn.ReLU(inplace=True),
+      nn.Conv2d(C // 2, C, 3, stride=2, padding=1, bias=False),
+      nn.BatchNorm2d(C),
+    )
+
+    self.stem1 = nn.Sequential(
+      nn.ReLU(inplace=True),
+      nn.Conv2d(C, C, 3, stride=2, padding=1, bias=False),
+      nn.BatchNorm2d(C),
+    )
+
+    self.vector_stem = nn.Sequential(
+      nn.Linear(vector_size, C),
+      nn.ReLU(inplace=True),
+    )
+
+    C_prev_prev, C_prev, C_curr = C, C, C
+
+    self.cells = nn.ModuleList()
+    reduction_prev = True
+    for i in range(layers):
+      if i in [layers // 3, 2 * layers // 3]:
+        C_curr *= 2
+        reduction = True
+        cell = Cell(genotype.reduce, genotype.reduce_concat, C_prev_prev, C_prev, C_curr, reduction, reduction_prev, op_dict=op_dict, C_mid=C_mid)
+      else:
+        reduction = False
+        cell = Cell(genotype.normal, genotype.reduce_concat, C_prev_prev, C_prev, C_curr, reduction, reduction_prev, op_dict=op_dict, C_mid=C_mid)
+      reduction_prev = reduction
+      self.cells += [cell]
+      C_prev_prev, C_prev = C_prev, cell.multiplier * C_curr
+      if i == 2 * layers // 3:
+        C_to_auxiliary = C_prev
+
+    if auxiliary:
+      self.auxiliary_head = AuxiliaryHeadImageNet(C_to_auxiliary, num_classes)
+    self.global_pooling = nn.AvgPool2d(7)
+    self.classifier = nn.Linear(C_prev, num_classes)
+
+  def forward(self, img, vector):
+    logits_aux = None
+    s0 = self.stem0(img)
+    s1 = self.stem1(s0)
+    v = self.vector_stem(vector)
+
+    s1.add_(v.unsqueeze(2).unsqueeze(3).expand(-1, -1, s1.shape[2], s1.shape[3]))
+
+    for i, cell in enumerate(self.cells):
+      s0, s1 = s1, cell(s0, s1, self.drop_path_prob)
+      if i == 2 * self._layers // 3:
+        if self._auxiliary and self.training:
+          logits_aux = self.auxiliary_head(s1)
+    out = self.global_pooling(s1)
+    logits = self.classifier(out.view(out.size(0), -1))
+    return logits, logits_aux
