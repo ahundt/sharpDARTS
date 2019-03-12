d1bda8c0a8fb8a105aa3e99fea678bfaeccc8caf
j-varun <vjain12@jhu.edu>
2019-03-12 00:06:17 -0400

    Fix for skip connections in graph

--- cnn/model_search.py
+++ cnn/model_search.py
@@ -365,8 +365,10 @@ class MultiChannelNetwork(nn.Module):
                   self.G.add_node(name)
                   if layer_idx == 0 and stride_idx == 0:
                     self.G.add_edge("BatchNorm_"+str(C_in_idx), name)
+                  else if stride_idx > 0:
+                    self.G.add_edge('layer_' + str(layer_idx)+'_add_' + 'c_out'+str(self.Cs[C_in_idx])+'_stride_' + str(stride_idx), name)
                   else:
-                    self.G.add_edge('layer_' + str(layer_idx-1)+'_add_' + 'c_out'+str(self.Cs[C_in_idx])+'_stride_' + str(self.strides[-1] + 1 if stride_idx == 0 else stride_idx), name)
+                    self.G.add_edge('layer_' + str(layer_idx-1)+'_add_' + 'c_out'+str(self.Cs[C_in_idx])+'_stride_' + str(self.strides[-1]), name)
                   self.G.add_edge(name, out_node)
                   op = OpType(int(cin), int(cout), kernel_size=3, stride=int(stride_idx + 1))
                   type_modules.append(op)
