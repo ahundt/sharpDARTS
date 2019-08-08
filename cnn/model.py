2b867bdeaa678cb79fe2498e6012f4575374526c
j-varun <vjain12@jhu.edu>
2019-08-08 18:50:10 -0400

    List operations replaced by numpy operations in model.py

--- cnn/model.py
+++ cnn/model.py
@@ -768,11 +768,11 @@ class MultiChannelNetworkModel(nn.Module):
           for C_out, C_ins in C_outs:
             # take all the layers with the same output so we can sum them
             # print('forward layer: ' + str(layer) + ' stride: ' + str(stride) + ' c_out: ' + str(self.Cs[C_out_idx]))
-            C_out_idx = self.Cs.index(C_out)
+            C_out_idx = np.where(self.Cs == C_out)
             c_outs = []
             # C_in_layer = [x[3] for x in self.inCs if x[0] == layer[0] and x[1] == strides[0] and x[2] == C_out]
             for C_in, primitives in enumerate(C_ins):
-              C_in_idx = self.Cs.index(C_in)
+              C_in_idx = np.where(self.Cs == C_in)
               for primitive_idx, primitive in primitives:
 
                 # get the specific weight for this op
